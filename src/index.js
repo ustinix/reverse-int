bac8d0e44c5fc670eca20a1c8e1499f612d31e89
Ustinix <ksuletters@yandex.ru>
2024-10-15 16:31:18 +0300

    init: reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n).toString()
+    let reverseNumber = "";
+    for(let i=n.length-1; i>=0; i--) {
+    reverseNumber += n[i]
+    }
+    return +reverseNumber
 }
